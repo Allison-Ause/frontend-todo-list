02be3371a66f1eb1a18250dc8153106f15781bf2
Allison Ause <allison.ause@gmail.com>
2022-08-23 14:25:53 -0700

    functioning route for sign in

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-import { signUpUser } from './utils/fetch-utils.js';
+import { signUpUser, signInUser } from './utils/fetch-utils.js';
 
 const signUpForm = document.getElementById('sign-up-form');
-const formData = new FormData(signUpForm);
+const signInForm = document.getElementById('sign-in-form');
 
 signUpForm.addEventListener('submit', async (e) => {
-    console.log('form functioning');
     e.preventDefault();
 
+    const formData = new FormData(signUpForm);
     await signUpUser({
         firstName: formData.get('first-name'),
         lastName: formData.get('last-name'),
+        email: formData.get('email'),
+        password: formData.get('password'),
+    });
+});
+
+signInForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    console.log('form functioning');
+    const formData = new FormData(signInForm);
+    await signInUser({
         email: formData.get('email'),
         password: formData.get('password'),
     });
--- utils/fetch-utils.js
+++ utils/fetch-utils.js
@@ -3,8 +3,27 @@
 const BASE_URL = 'http://localhost:7890';
 
 export async function signUpUser(userInfo) {
-    console.log('firing');
     const res = await fetch(`${BASE_URL}/api/v1/users`, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+        body: JSON.stringify(userInfo),
+    });
+    const data = await res.json();
+    if (res.ok) {
+        location.replace('./todos');
+    } else {
+        // eslint-disable-next-line no-console
+        console.error(data.message);
+    }
+}
+
+export async function signInUser(userInfo) {
+    console.log('firing');
+    const res = await fetch(`${BASE_URL}/api/v1/users/sessions`, {
         method: 'POST',
         headers: {
             Accept: 'application/json',
