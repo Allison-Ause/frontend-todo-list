43ad007915fe67e898e0f9c575cdde09e1628487
Allison Ause <allison.ause@gmail.com>
2022-08-22 17:28:14 -0700

    fleshed out basic html and started signupform functionality

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-// import functions and grab DOM elements
+import { signUpUser } from './utils/fetch-utils';
 
-// let state
+const signUpForm = document.querySelector('form');
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(signUpForm);
+    const data = await signUpUser({
+        firstName: formData.get('first-name'),
+        lastName: formData.get('last-name'),
+        email: formData.get('email'),
+        password: formData.get('password'),
+    });
+});
---
+++ utils/fetch-utils.js
@@ -0,0 +1,1 @@
+// create signUpUser function
