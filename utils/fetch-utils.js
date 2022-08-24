a3b80b4cbaa9db5783647d290b3b4f6c4af9d4f7
Allison Ause <allison.ause@gmail.com>
2022-08-23 17:45:44 -0700

    updated the base url to heroku deploy

--- utils/fetch-utils.js
+++ utils/fetch-utils.js
@@ -1,6 +1,6 @@
 // create signUpUser function
 
-const BASE_URL = 'http://localhost:7890';
+const BASE_URL = 'https://aause-backend-todo-list.herokuapp.com/';
 
 export async function signUpUser(userInfo) {
     const res = await fetch(`${BASE_URL}/api/v1/users`, {
